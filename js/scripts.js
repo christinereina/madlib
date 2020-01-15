78501db069c3770242daccf6d6e5a8f894965a31
Jaki Widby and Keturah Howard <keturah@>
2020-01-15 09:48:53 -0800

    placed jquery $ correctly in the function

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1 = ("input#person1").val()
-    var person2 = ("input#person2").val()
-    var animal = ("input#anamal").val()
-    var exclamation = ("input#exclamation").val()
-    var verb = ("input#verb").val()
-    var noun = ("input#noun").val()
+    var person1 = $("input#person1").val();
+    var person2 = $("input#person2").val();
+    var animal = $("input#animal").val();
+    var exclamation = $("input#exclamation").val();
+    var verb = $("input#verb").val();
+    var noun = $("input#noun").val();
+
+
+    // var person1 = "ohhhh";
+
+    
+    // $(".person1").text(person1);
     
     $(".person1").text(person1);
     $(".person2").text(person2);
@@ -18,3 +24,4 @@ $(document).ready(function() {
     event.preventDefault();
   });
 });
+
